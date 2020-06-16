0d2459cc3dffb479a34544519ca483416336f72c
vecume <55271579+vecume@users.noreply.github.com>
2020-06-17 00:13:35 +0500

    Fix problems with auto scrolling

--- app/js/index.js
+++ app/js/index.js
@@ -2190,7 +2190,7 @@ $(document).ready(function () {
             "background-image": "url('img/icons/text.png')"
         });
         $('.tabs').animate({
-            scrollLeft: 250
+            scrollLeft: 450
         },300)
     });
 
